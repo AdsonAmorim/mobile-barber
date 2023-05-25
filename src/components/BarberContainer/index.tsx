36456c80c0831807c6ef5c3134a80d583bf262e6
Adson Amorim <adson.nascimento@wisereducacao.com>
2023-05-25 01:19:44 -0300

    feat: add  navigation with barber id

--- src/components/BarberContainer/index.tsx
+++ src/components/BarberContainer/index.tsx
@@ -3,13 +3,14 @@ import { Text, View, TouchableOpacity } from "react-native";
 import { Image } from "expo-image";
 
 import Icon from "react-native-vector-icons/AntDesign";
+import { useNavigation } from "@react-navigation/native";
 
 interface BarberContainerProps {
+  id: string;
   name: string;
   calendar: string;
   avatar_url: string;
   hour: string;
-  navigation: any;
 }
 
 const BarberContainer = ({
@@ -17,16 +18,18 @@ const BarberContainer = ({
   calendar,
   hour,
   name,
-  navigation,
+  id,
 }: BarberContainerProps) => {
+  const navigation = useNavigation();
+
   const handlePress = () => {
-    navigation.navigate("Calendar");
+    navigation.navigate("Calendar", { userId: id });
   };
 
   return (
     <TouchableOpacity onPress={handlePress}>
-      <View className="bg-gray-800 text-neutral-50 w-full  max-w-90 h-[112] rounded-md flex flex-row items-center gap-x-6">
-        <View className="w-[72] h-[72]  rounded-full">
+      <View className=" bg-gray-800 border text-neutral-50  h-[112] rounded-md mt-5 flex items-center flex-row justify-around">
+        <View className="w-[72] h-[72] rounded-full">
           <Image
             source={{
               uri: avatar_url,
@@ -34,16 +37,16 @@ const BarberContainer = ({
             className="w-full h-full rounded-full"
           />
         </View>
-        <View className="w-6/12 flex flex-col gap-y-1 m-0  p-0">
+        <View className="w-6/12 flex flex-col gap-y-0 m-0 p-0">
           <Text className="text-neutral-50 font-bold text-lg">{name}</Text>
 
           <View className="flex items-center justify-start flex-row gap-x-2">
-            <Icon name="calendar" color="#FFF" size="20" />
+            <Icon name="calendar" color="#FFF" size={20} />
             <Text className="text-neutral-50 font-medium">{calendar}</Text>
           </View>
 
           <View className="flex items-center justify-start flex-row gap-x-2">
-            <Icon name="clockcircleo" color="#FFF" size="20" />
+            <Icon name="clockcircleo" color="#FFF" size={20} />
             <Text className="text-neutral-50 font-medium">{hour}</Text>
           </View>
         </View>
