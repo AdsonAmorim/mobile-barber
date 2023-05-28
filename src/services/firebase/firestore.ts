f41b861694d56274907a0b798f96b6b2f508a761
Adson Amorim <adson.nascimento@wisereducacao.com>
2023-05-27 21:49:56 -0300

    feat: update firestore config

--- src/services/firebase/firestore.ts
+++ src/services/firebase/firestore.ts
@@ -1,4 +1,4 @@
 import { firebaseApp } from "./config";
-import { getFirestore } from "firebase/firestore/lite";
+import { getFirestore } from "firebase/firestore";
 
 export const db = getFirestore(firebaseApp);
