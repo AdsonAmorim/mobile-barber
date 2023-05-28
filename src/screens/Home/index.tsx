df715d7e728e44a1d74bcaf33a0dd50e16f0eacc
Adson Amorim <adson.nascimento@wisereducacao.com>
2023-05-27 21:49:24 -0300

    feat: update firebase config

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,28 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Text, View } from "react-native";
 import { BarberContainer } from "../../components/BarberContainer";
 import { Header } from "../../components/Header";
 
+import { getBarbers } from "../../services/barbers";
+import { BarberContainerProps } from "../../types";
+
 import { HomeScreenNavigationProp, HomeScreenRouteProp } from "./types";
 
-const mockBarbers = [
-  {
-    id: "1",
-    avatar_url:
-      "https://images.unsplash.com/photo-1517849845537-4d257902454a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=735&q=80",
-    name: "Sêneca",
-    calendar: "Segunda à sábado",
-    hour: "8h às 18h",
-  },
-  {
-    id: "2",
-    avatar_url:
-      "https://images.unsplash.com/photo-1541364983171-a8ba01e95cfc?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80",
-    name: "Marco Aurélio",
-    calendar: "Segunda à sábado",
-    hour: "8h às 18h",
-  },
-];
+// const mockBarbers = [
+//   {
+//     id: "1",
+//     avatar_url:
+//       "https://images.unsplash.com/photo-1517849845537-4d257902454a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=735&q=80",
+//     name: "Sêneca",
+//     calendar: "Segunda à sábado",
+//     hour: "8h às 18h",
+//   },
+//   {
+//     id: "2",
+//     avatar_url:
+//       "https://images.unsplash.com/photo-1541364983171-a8ba01e95cfc?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80",
+//     name: "Marco Aurélio",
+//     calendar: "Segunda à sábado",
+//     hour: "8h às 18h",
+//   },
+// ];
 
 interface HomeProps {
   navigation: HomeScreenNavigationProp;
@@ -30,6 +33,19 @@ interface HomeProps {
 }
 
 const HomeScreen = ({ navigation, route }: HomeProps) => {
+  const [barbers, setBarbers] = useState<BarberContainerProps[]>([]);
+
+  const getBarbersFromDatabase = async () => {
+    const barbersFromDatabase = await getBarbers();
+    setBarbers(barbersFromDatabase);
+  };
+
+  useEffect(() => {
+    if (!barbers.length) {
+      getBarbersFromDatabase();
+    }
+  }, []);
+
   return (
     <View className="box-border flex-1 p-0 m-0 bg-gray-950">
       <Header showBackButton={false} />
@@ -37,8 +53,8 @@ const HomeScreen = ({ navigation, route }: HomeProps) => {
         <Text className="mt-5 text-3xl font-medium text-neutral-50">
           Barbeiros
         </Text>
-        {mockBarbers.map((mockBarber) => (
-          <BarberContainer {...mockBarber} key={mockBarber.id} />
+        {barbers.map((barber) => (
+          <BarberContainer {...barber} key={barber.id} />
         ))}
       </View>
     </View>
--- src/services/barbers/index.ts
+++ src/services/barbers/index.ts
@@ -1,12 +1,16 @@
-import { collection, getDocs } from "firebase/firestore/lite";
+import { collection, getDocs } from "firebase/firestore";
+import { BarberContainerProps } from "../../types";
 import { db as dataBase } from "../firebase/firestore";
 
 const getBarbers = async () => {
   const barbersColumn = collection(dataBase, "barbers");
   const barbersSnapshot = await getDocs(barbersColumn);
-  const barbersList = barbersSnapshot.docs.map((doc) => doc.data);
+  const barbersList = barbersSnapshot.docs.map((snapshot) => ({
+    ...(snapshot.data() as BarberContainerProps),
+    id: snapshot.id,
+  }));
 
-  console.log("@barber", barbersList);
+  console.log("@barbers", JSON.stringify(barbersList));
 
   return barbersList;
 };
